3c32908873e4f43e9cb9ca24f9d0c04f64151555
cwts007 <116644442+cwts007@users.noreply.github.com>
2024-05-03 13:18:08 -0400

    sE AGREGAN lOGOS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import logo from '../assets/images/logo.png'; // Importa la imagen desde la carpeta de assets
 
 const Header = ({ usuario }) => {
     return (
         <Navbar bg="light" expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">Registro Servicio</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Logo"
+                    />{' '}
+                    Registro Servicio
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                     <Navbar.Text>
